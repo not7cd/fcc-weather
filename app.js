e3d6e1d26859bb285af5be30dfd9b5e434810736
not7CD <norbertszulc13@gmail.com>
2017-07-06 21:53:49 +0200

    Move to VanillaJS

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-var app = new Vue({
-  el: "#card",
-  data: {
-    location: "Farmville",
-    temperature: "9000C"
-  }
-});
+var apiKey = "9239469f010d7bb17fcc5de5107d9852";
+// TODO: Move global vars to objects
+var WEATHER = {};
+
+function updateData(newData) {
+  WEATHER = newData;
+  console.log(WEATHER);
+}
+
+
+function getWeather(key) {
+  var url = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139" + "&appid=" + key;
+  var http_weather = new XMLHttpRequest();
+  http_weather.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      updateData(response);
+      // updateDom(WEATHER);
+    }
+  };
+  http_weather.open("GET", url, true);
+  http_weather.send();
+}
+
+getWeather(apiKey);
